8e1006a50f65950ba75e0c5dd72d83714e14e17e
jwalapj0216 <jwalapj02@gmail.com>
2026-04-08 09:34:22 +0530

    Refactor deploy script to use docker-compose
    Updated deployment script to use docker-compose for managing containers and added cleanup for unused images.

--- Script/deploy.sh
+++ Script/deploy.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+echo "-------------------------------------------------"
+echo "-------------deploy------------------------------"
+echo "-------------------------------------------------"
 
-IMAGE_NAME="jwalapj02/devops-build-dev:tagname"
+echo "Image: $IMAGE"
+echo "Port: $PORT"
 
 echo "Pulling latest image..."
 docker pull $IMAGE_NAME
 
-echo "Stopping existing container..."
-docker stop react-container || true
-docker rm react-container || true
+echo " Stopping old containers..."
+docker-compose down || true
 
-echo "Running new container..."
-docker run -d -p 3000:3000 --name react-container $IMAGE_NAME
+echo " Starting new containers..."
+docker-compose up -d
+
+sleep 15
+
+if curl -f http://localhost:$PORT > /dev/null 2>&1; then
+  echo " App is running successfully on port $PORT"
+else
+  echo " App failed to start"
+  docker-compose logs
+  exit 1
+fi
+
+#  CLEANUP
+echo " Cleaning up unused images..."
+docker image prune -f
 
 echo "Deployment successful!"
+echo "-------------------------------------------------"
